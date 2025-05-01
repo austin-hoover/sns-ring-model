9b7735cb01b2be5fa98a453b54340be33269a2cd
austin-hoover <austin.michael.hoover@gmail.com>
2025-05-01 12:45:19 -0400

    Add get_intensity function to full injection benchmark

--- scripts/full_injection_benchmark/utils.py
+++ scripts/full_injection_benchmark/utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 
+def get_intensity(power: float, energy: float, frequency: float) -> float:
+    joules_per_ev = 1.6022e-19
+    energy_per_pulse = power / frequency / joules_per_ev
+    intensity = energy_per_pulse / energy
+    return intensity
+    
 def read_transverse_impedance_file(path: str) -> tuple[list[complex]]:
     zp, zm = [], []
     for m, zp_real, zp_imag, zm_real, zm_imag in np.loadtxt(path, comments="#"):
